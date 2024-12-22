5bd59197774cf4e8b6927d7ab1d08d5a14bd41b5
Tofiq041t <huseynlitofiq2005@gmail.com>
2024-12-22 15:02:15 +0400

    Add new option - forgot password, whenever system starts

--- RealEstateApp.java
+++ RealEstateApp.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class RealEstateApp {
 
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.println("===========================================");
             System.out.println("Welcome to PARALLAX Real Estate Application");
             System.out.println("===========================================");
             System.out.println("1. Login");
             System.out.println("2. Register");
-            System.out.println("3. Exit");
-            System.out.print("Choose an Option: ");
-            String option = scanner.nextLine();
+            System.out.println("3. Forgot Password");
+            System.out.println("4. Exit");
+            System.out.println("\nChoose an Option: ");
+            String option = scanner.next();
 
             switch (option) {
                 case "1":
@@ -23,14 +25,15 @@ public class RealEstateApp {
                     LoginSystem.register();
                     break;
                 case "3":
-                    System.out.println("Exiting... Thank you!");
+                    LoginSystem.forgotPassword();
+                    break;
+                case "4":
+                    System.out.println("\nExiting... Thank you!");
+                    scanner.close();
                     System.exit(0);
                 default:
                     System.out.println("Invalid Option. Please Try Again.");
             }
         }
     }
-
-
-   
 }
